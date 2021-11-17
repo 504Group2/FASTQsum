307a6dff606ffe0dff50b62f34832de4ad64229d
NaphatCode <naphat.spakdee@gmail.com>
2021-11-17 18:30:16 +0700

    can generate html

--- report/summary/feature.py
+++ report/summary/feature.py
@@ -1,5 +1,6 @@
 from report.read.readFastq import fqToCsv
-
+import pandas as pd
+import plotly.express as px
 #16nov
 
 def lenSum(csv):
@@ -8,6 +9,10 @@ def scSum(csv):
     print("This is score summary")
 def scVsLen(csv):
     print("This is score vs summary summary")
+    c=pd.read_csv(csv)
+    print (pd.DataFrame.head(c))
+    fig =px.scatter(x=range(10), y=range(10))
+    fig.write_html("../file.html")
 
 def csvToHtml(csv):
 
