7043099e06eb63d7ff4719f6e47cf33c5f601112
Bowornpol Nu-im <88828798+Bowornpol@users.noreply.github.com>
2021-11-20 08:15:28 +0700

    Update feature.py
    Adding the quality score feature

--- report/summary/feature.py
+++ report/summary/feature.py
@@ -1,12 +1,40 @@
 from report.read.readFastq import fqToCsv
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
 #16nov
 
 def lenSum(csv):
     print("This is read length summary")
 def scSum(csv):
     print("This is score summary")
+    sum = pd.read_csv(csv)
+    #Summary table
+    qsummarytable = sum.groupby('Barcode_arrangement').describe()
+    print(qsummarytable)
+    
+    #Basecalled reads PHRED quality
+    qfig = ff.create_distplot([sum[sum['Barcode_arrangement']=='barcode02']['Mean_qscore_template'],
+                         sum[sum['Barcode_arrangement']=='barcode01']['Mean_qscore_template']], 
+                         ['barcode02', 'barcode01'],
+                         colors = ['#F66095', '#2BCDC1'],
+                         show_hist=False
+                        )
+    qfig.update_layout(title="Basecalled reads PHRED quality", xaxis_title="Reads quality scores", yaxis_title="Read density", legend_title="Barcode")
+    qfig.add_vline(x=8.0, line_width=1.5, line_dash="dash", line_color="red", annotation_text="Cut-off line", annotation_font_color="red")
+
+    #Number of reads per quality score
+    PassReads =  sum.query('Mean_qscore_template >= 8')
+    FailReads =  sum.query('Mean_qscore_template < 8')
+
+    qlabels = ['Pass Reads','Fail Reads']
+    qvalues = [len(PassReads), len(FailReads)]
+
+    qPiefig = go.Figure(data=[go.Pie(labels=qlabels, values=qvalues, textinfo='label+percent',
+                             insidetextorientation='radial', pull=[0, 0.2]
+                            )])
+    qPiefig.update_layout(title="Number of reads per quality score", legend_title="Type of reads")    
 def scVsLen(csv):
     #Bam
     print("This is score vs summary summary")
