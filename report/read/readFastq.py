ca64976d8b25f026712b920af3398dcf99ddcffd
NaphatCode <65748521+NaphatCode@users.noreply.github.com>
2021-12-01 20:38:22 +0700

    remove start_time

--- report/read/readFastq.py
+++ report/read/readFastq.py
@@ -11,49 +11,49 @@ from Bio import SeqIO
 #4.series to dataframe  
 
 #1.Read fastq 
-# Extract,Read,Parse ont.exp2.fastq.gz into list bc01
+# Extract,Read,Parse ont.exp2.fastq.gz into list seqList
 def getFq(filePath) :
     
-    bc01=[]
+    seqList=[]
     with gzip.open(filePath,'rt') as f:
     #with gzip.open('../fastq/ont.exp2.fastq.gz','rt') as f: # Decompress ont.exp2.fastq.gz 
         for idx, seq_record in enumerate(SeqIO.parse(f, "fastq")): # Read and parse 
-            bc01.append(seq_record)
-            if idx == 200000: # set how many reads we want
+            seqList.append(seq_record)
+            if idx == 1000: # set how many reads we want
                 break
-    return bc01
+    return seqList
 #2.seq_record object to lists             
-# Breakdown list bc01 into lists of each column
+# Breakdown list seqList into lists of each column
 # List for column Read_id   
-def colToList(bc01): 
-    index=range(len(bc01))
+def colToList(seqList): 
+    index=range(len(seqList))
     idcodel=[]
-    for i in bc01:
+    for i in seqList:
         idcodel.append(i.id)
 
 # List for column Sequence_length_template
     seql = []
-    for i in bc01:
+    for i in seqList:
         seql.append(len(i.seq))
 
 # description from fastq is a string
 # split string into 2 columns: Start_time, Barcode_arrangement
     descriptionl=[]
-    start_timel=[]
+    
     barcodel=[]
-    for i in bc01:
+    for i in seqList:
         descriptionl.append(i.description)
    
     for des in descriptionl:
         dl=des.split()
-        start_timel.append(dl[5][11:])
+       
         barcodel.append(dl[6][8:])
 
 # List for column Mean_qscore_template
     qual = []
-    for i in bc01:
+    for i in seqList:
         qual.append(np.mean(i.letter_annotations['phred_quality']))
-        mycolList=[index,idcodel,start_timel,seql,qual,barcodel]
+        mycolList=[index,idcodel,seql,qual,barcodel]
     return mycolList
 
 
@@ -63,14 +63,14 @@ def listToDf(mycolList):
 #   
     indexse = pd.Series(mycolList[0], name='index')
     readid = pd.Series(mycolList[1], name='Read_ID')
-    start_times = pd.Series(mycolList[2], name='Start_time')
+    
     seqs = pd.Series(mycolList[3], name='Sequence_length_template')
     quality = pd.Series(mycolList[4], name='Mean_qscore_template')
     barcodes = pd.Series(mycolList[5], name='Barcode_arrangement')
 
 #4.series to dataframe 
 
-    fastqdf=pd.DataFrame(dict(Index=indexse,Read_ID=readid,Start_time=start_times,Sequence_length_template=seqs,Mean_qscore_template=quality,Barcode_arrangement=barcodes)).set_index(['Index'])
+    fastqdf=pd.DataFrame(dict(Index=indexse,Read_ID=readid,Sequence_length_template=seqs,Mean_qscore_template=quality,Barcode_arrangement=barcodes)).set_index(['Index'])
     return fastqdf
 
 #5.convert dataframe to csv file
@@ -83,8 +83,8 @@ def dfToCsv(fastqdf):
     return csvLocation
 
 def fqToCsv(filePath):
-    bc01=getFq(filePath)
-    newcolList=colToList(bc01)
+    seqList=getFq(filePath)
+    newcolList=colToList(seqList)
     mydf=listToDf(newcolList)
     print(dfToCsv(mydf))
     return dfToCsv(mydf) #.csv location
